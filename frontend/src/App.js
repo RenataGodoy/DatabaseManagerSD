a081f1bd72ad12a34c98f79cba9ae8f867497616
renatag <devrenatagodoy@gmail.com>
2025-03-12 13:13:09 -0300

    Added Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,12 @@ import CreateDatabasePage from './pages/CreateDatabasePage';
 import CreateTablePage from './pages/CreateTablePage';
 import InsertRecordPage from './pages/InsertRecordPage';
 import ListRecordsPage from './pages/ListRecordsPage';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <div>
-        <h1>Ferramenta Administrativa de Banco de Dados</h1>
+      <div className="App">
         <nav>
           <ul>
             <li><Link to="/">Início</Link></li>
@@ -21,17 +21,19 @@ function App() {
           </ul>
         </nav>
 
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/create-database" element={<CreateDatabasePage />} />
-          <Route path="/create-table" element={<CreateTablePage />} />
-          <Route path="/insert-record" element={<InsertRecordPage />} />
-          <Route path="/list-records" element={<ListRecordsPage />} />
-        </Routes>
+        <div className="App-header">
+          <h1>Ferramenta Administrativa de Banco de Dados</h1>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/create-database" element={<CreateDatabasePage />} />
+            <Route path="/create-table" element={<CreateTablePage />} />
+            <Route path="/insert-record" element={<InsertRecordPage />} />
+            <Route path="/list-records" element={<ListRecordsPage />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
 }
 
 export default App;
-
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import '../style.css';
 
 function HomePage() {
   return (
     <div>
       <h2>Bem-vindo à Ferramenta Administrativa de Banco de Dados</h2>
-      <p>Escolha uma das opções no menu para começar.</p>
+      <p>Aqui voce pode criar o banco de dados, tanto quanto criar a tabela e visualizar os dados.</p>
+      <p>Use o menu lateral para escolher qual ação quer tomar</p>
     </div>
   );
 }
