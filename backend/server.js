a7288dc551ff111a5e7f2d9be17f5c6c77671144
RenataGodoy <devrenatagodoy@gmail.com>
2025-04-02 20:42:37 -0300

    last commit

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,20 @@ const app = express();
 const port = 5000;
 
 const cors = require('cors');
-app.use(cors());  // Habilita o CORS para permitir requisições de outros domínios
+app.use(cors());  // Habilita o CORS
 
-// Criação da conexão com o MySQL
+// conexão com o MySQL
 const db = mysql.createConnection({
   host: 'localhost',
-  user: 'root',  // Use suas credenciais
-  password: 'root',  // Use sua senha do MySQL
+  user: 'root',  
+  password: 'root',
 });
 
 db.connect((err) => {
   if (err) {
     console.error('Erro de conexão com o banco de dados:', err);
   } else {
-    console.log('Conectado ao banco de dados MySQL');
+    console.log('Conectado ao banco de dados');
   }
 });
 
@@ -25,10 +25,11 @@ app.use(express.json());
 
 
 app.get('/', (req, res) => {
-    res.send('Servidor está funcionando corretamente!');
+    res.send('Servidor está funcionando certinho!');
   });
   
-// Rota para criar um banco de dados
+
+// Rota -->
 app.post('/create-database', (req, res) => {
   const { dbName } = req.body;
   const query = `CREATE DATABASE IF NOT EXISTS ${dbName}`;
@@ -37,11 +38,11 @@ app.post('/create-database', (req, res) => {
     if (err) {
       return res.status(500).json({ message: 'Erro ao criar banco de dados', error: err });
     }
-    res.status(200).json({ message: `Banco de dados ${dbName} criado com sucesso!` });
+    res.status(200).json({ message: `Banco de dados ${dbName} criado com sucesso!!!!!` });
   });
 });
 
-//Rota para pegar os databases para aparecer no drop down
+//Rota -->
 app.get('/show-databases', (req, res) => {
     const query = 'SHOW DATABASES';
   
@@ -54,7 +55,7 @@ app.get('/show-databases', (req, res) => {
   });
 
 
-  // Rota para listar tabelas de um banco de dados
+  // Rota -->
 app.get('/show-tables', (req, res) => {
     const { dbName } = req.query;
     const query = `SHOW TABLES FROM ${dbName}`;
@@ -67,7 +68,7 @@ app.get('/show-tables', (req, res) => {
     });
   });
   
-// Rota para obter as colunas de uma tabela
+// Rota -->
 app.get('/show-columns', (req, res) => {
     const { dbName, tableName } = req.query;
     db.query(`DESCRIBE ${dbName}.${tableName}`, (err, result) => {
@@ -76,7 +77,7 @@ app.get('/show-columns', (req, res) => {
     });
   });
   
-// Rota para criar uma tabela em um banco de dados
+// Rota ==>
 app.post('/create-table', (req, res) => {
   const { dbName, tableName, columns } = req.body;
   const query = `CREATE TABLE IF NOT EXISTS ${dbName}.${tableName} (${columns})`;
@@ -89,7 +90,7 @@ app.post('/create-table', (req, res) => {
   });
 });
 
-// Rota para inserir registros
+// Rota -->
 app.post('/insert-record', (req, res) => {
   const { dbName, tableName, values } = req.body;
   const query = `INSERT INTO ${dbName}.${tableName} VALUES (${values})`;
@@ -102,7 +103,7 @@ app.post('/insert-record', (req, res) => {
   });
 });
 
-// Rota para listar registros
+// Rota -->
 app.get('/list-records', (req, res) => {
   const { dbName, tableName } = req.query;
   const query = `SELECT * FROM ${dbName}.${tableName}`;
@@ -115,7 +116,7 @@ app.get('/list-records', (req, res) => {
   });
 });
 
-// Inicia o servidor
+// Inicia
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -5,7 +5,7 @@ function HomePage() {
   return (
     <div>
       <h2>Bem-vindo à Ferramenta Administrativa de Banco de Dados</h2>
-      <p>Aqui voce pode criar o banco de dados, tanto quanto criar a tabela e visualizar os dados.</p>
+      <p>Nesta pagina voce pode criar o banco de dados, tanto quanto criar a tabela e visualizar os dados.</p>
       <p>Use o menu lateral para escolher qual ação quer tomar</p>
     </div>
   );
